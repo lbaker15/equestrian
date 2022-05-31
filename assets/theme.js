dad0ca63f8d73d418b9e01d710dff777eeae7a6c
lbaker15 <laelbaker@hotmail.co.uk>
2022-05-31 13:57:02 +0100

    h

--- assets/theme.js
+++ assets/theme.js
@@ -4005,20 +4005,26 @@ var afterResize = (function () {
 window.addEventListener('DOMContentLoaded', () => {
   let menu = document.querySelector('.header__menu');
   let list = document.querySelector('.header__menu-list');
+  
+  //BURGER MENU
   let state = false;
-  menu.addEventListener('click', () => {
-    if (!state) {
-      list.style.display = 'block'
-      setTimeout(() => list.style.transform = 'translateX(0vw)', 1)
-      document.body.style.overflowY = 'hidden'
-      state = true;
-    } else {
-      setTimeout(() => list.style.display = 'none', 500)
-      list.style.transform = 'translateX(-100vw)'
-      document.body.style.overflowY = 'scroll'
-      state = false;
-    }
-  })
+  if (menu) {
+    menu.addEventListener('click', () => {
+      if (!state) {
+        list.style.display = 'block'
+        setTimeout(() => list.style.transform = 'translateX(0vw)', 1)
+        document.body.style.overflowY = 'hidden'
+        menu.classList.add('js-drawer-open');
+        state = true;
+      } else {
+        setTimeout(() => list.style.display = 'none', 500)
+        list.style.transform = 'translateX(-100vw)'
+        document.body.style.overflowY = 'scroll'
+        menu.classList.remove('js-drawer-open');
+        state = false;
+      }
+    })
+  }
 
   let prodSize = document.getElementById('prod-size');
   let prodSizeH2 = document.querySelector('#prod-size h2');
@@ -4038,7 +4044,7 @@ window.addEventListener('DOMContentLoaded', () => {
       if (e.target === prodSizeH2) {
         let tl = gsap.timeline({ paused: true })
         if (!Array.from(prodSize.classList).find(item => item === 'no-stock')) {
-        tl.fromTo(prodSize, { borderLeft: '2px solid black', borderRight: '2px solid black' }, { borderLeft: '0', borderRight: '0', duration: 0.3, ease: 'easeOut' })
+          tl.fromTo(prodSize, { borderLeft: '2px solid black', borderRight: '2px solid black' }, { borderLeft: '0', borderRight: '0', duration: 0.3, ease: 'easeOut' })
         } else {
           tl.fromTo(prodSize, { borderLeft: '1px solid grey', borderRight: '1px solid grey' }, { borderLeft: '0', borderRight: '0', duration: 0.3, ease: 'easeOut' })
         }
@@ -4061,7 +4067,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
   if (prodCart) {
     prodCart.addEventListener('click', () => {
-      //let id = selectedV.value; 
       let inventory = selectedV.dataset.value;
       //Check if inventory is enough before request
       let id = 42959644623077;
@@ -4072,7 +4077,7 @@ window.addEventListener('DOMContentLoaded', () => {
         credentials: 'same-origin',
         headers: {
           'Content-Type': 'application/json',
-          'X-Requested-With': 'xmlhttprequest' /* XMLHttpRequest is ok too, it's case insensitive */
+          'X-Requested-With': 'xmlhttprequest'
         },
         method: 'POST'
       }).then(function (response) {
@@ -4080,7 +4085,6 @@ window.addEventListener('DOMContentLoaded', () => {
       }).then(function (json) {
         /* we have JSON */
         if (json) {
-          console.log(json)
           let add = document.getElementById('add-c')
           let added = document.getElementById('added-c')
           console.log(added)
@@ -4088,7 +4092,6 @@ window.addEventListener('DOMContentLoaded', () => {
           added.style.display = 'flex';
         }
       }).catch(function (err) {
-        /* uh oh, we have error. */
         console.error(err)
       });
     })
@@ -4100,27 +4103,33 @@ window.addEventListener('DOMContentLoaded', () => {
   let tl2 = gsap.timeline({ paused: true })
   tl2.fromTo(popOverlay, { display: 'none', opacity: 0 }, { opacity: 1, display: 'flex', duration: 0.02 })
   tl2.fromTo('.size-guide-pop', { display: 'none', opacity: 0 }, { opacity: 1, display: 'flex', duration: 0.2 })
-  sizeGuide.addEventListener('click', () => {
-    if (!sizePopState) {
-      tl2.restart(0)
-      sizePopState = true;
-    } else if (sizePopState) {
+  if (sizeGuide) {
+    sizeGuide.addEventListener('click', () => {
+      if (!sizePopState) {
+        tl2.restart(0)
+        sizePopState = true;
+      } else if (sizePopState) {
+        tl2.reverse(0)
+        sizePopState = false;
+      }
+    })
+  }
+  if (popSizeClose) {
+    popSizeClose.addEventListener('click', () => {
       tl2.reverse(0)
       sizePopState = false;
-    }
-  })
-  popSizeClose.addEventListener('click', () => {
-    tl2.reverse(0)
-    sizePopState = false;
-  })
+    })
+  }
 
   let arr = [prodDets, garCare, shipping];
   let arrSections = [prodDetsSection, garCareSection, shippingSection]
-  arr.map((item, i) => {
-    item.addEventListener('click', () => {
-      gsap.to(window, { duration: 0.5, scrollTo: arrSections[i] })
+  if (arr) {
+    arr.map((item, i) => {
+      item.addEventListener('click', () => {
+        gsap.to(window, { duration: 0.5, scrollTo: arrSections[i] })
+      })
     })
-  })
+  }
 
   let prodImgCarousel = Array.from(document.querySelectorAll('.product__hero-c-img'));
   let prodHeroImg = document.querySelector('.product__hero-img')
@@ -4129,53 +4138,92 @@ window.addEventListener('DOMContentLoaded', () => {
   let scroller = document.querySelector('.product__hero-scroller-in')
   let pl = document.getElementById('prod-left');
   let pr = document.getElementById('prod-right');
+  let carouselL = document.querySelector('.product-hero__carousel-l')
+  let carouselR = document.querySelector('.product-hero__carousel-r')
   let count = 1;
-  gsap.set('.product__hero-carousel-inner', { width: (prodImgCarousel.length * 85) - 25 })
-  prodImgCarousel.map((item, i) => {
-    item.addEventListener('click', (e) => {
-      let img = (e.target.querySelector('div').dataset.value);
-      prodHeroImg.style.backgroundImage = `url('${img}')`
+  gsap.set('.product__hero-carousel-inner', { width: (prodImgCarousel.length * (83)) - 46 })
+  if (prodImgCarousel.length) {
+    prodImgCarousel.map((item, i) => {
+      item.addEventListener('click', (e) => {
+        count = i + 1;
+        let img = (e.target.querySelector('div').dataset.value);
+        prodHeroImg.style.backgroundImage = `url('${img}')`
+        let len = prodImgCarousel.length;
+        let gap = 100 / len;
+        let val = (i === len - 1) ? ((i + 1) * gap) + 20 : (i + 1) * gap
+        gsap.to(scroller, { width: val + '%' })
+      })
+    })
+  }
+  if (carouselL) {
+    carouselL.addEventListener('click', (e) => {
+      count -= 1;
+      if (count === 0) {
+        count = prodImgCarousel.length;
+      }
+      let img = (prodImgCarousel[count - 1]) ? prodImgCarousel[count - 1].querySelector('div').dataset.value : '';
+      prodHeroImg.style.backgroundImage = `url('${img}')`;
       let len = prodImgCarousel.length;
       let gap = 100 / len;
-      console.log(len, gap, i + 1 * gap)
-      let val = (i === len - 1) ? ((i + 1) * gap) + 20 : (i + 1) * gap
+      let val = (count === len) ? ((count) * gap) + 20 : (count) * gap
       gsap.to(scroller, { width: val + '%' })
     })
-  })
+  }
+  if (carouselR) {
+    carouselR.addEventListener('click', (e) => {
+      count += 1;
+      if (count === prodImgCarousel.length + 1) {
+        count = 1;
+      }
+      let img = prodImgCarousel[count - 1].querySelector('div').dataset.value;
+      prodHeroImg.style.backgroundImage = `url('${img}')`;
+      let len = prodImgCarousel.length;
+      let gap = 100 / len;
+      let val = (count === len) ? ((count) * gap) + 20 : (count) * gap
+      gsap.to(scroller, { width: val + '%' })
+    })
+  }
   let tlOverlay = gsap.timeline({ paused: true })
   tlOverlay.fromTo(prodHeroImgOverlay, { display: 'none' }, { display: 'flex' })
   tlOverlay.fromTo(prodHeroImgOverlay, { opacity: 0 }, { opacity: 1, duration: 0.3 })
   tlOverlay.fromTo(document.body, { overflowY: 'scroll' }, { overflowY: 'hidden' }, '<')
   let prodImgOverlay = false;
-  prodHeroImg.addEventListener('click', () => {
-    if (!prodImgOverlay) {
-      tlOverlay.restart(0)
-
-      prodImgOverlay = true;
-    } else {
-      tlOverlay.reverse(0)
-
+  if (prodHeroImg) {
+    prodHeroImg.addEventListener('click', () => {
+      if (!prodImgOverlay) {
+        tlOverlay.restart(0)
+        prodImgOverlay = true;
+      } else {
+        tlOverlay.reverse(0)
+        prodImgOverlay = false;
+      }
+    })
+  }
+  if (prodHeroImgOut) {
+    prodHeroImgOut.addEventListener('click', () => {
+      tlOverlay.reverse()
       prodImgOverlay = false;
-    }
-  })
-  prodHeroImgOut.addEventListener('click', () => {
-    tlOverlay.reverse()
-    prodImgOverlay = false;
-  })
-  pl.addEventListener('click', () => {
-    let a = Array.from(document.querySelector('.get-imgs').querySelectorAll('img'));
-    count -= 1;
-    if (count === 0) {
-      count = a.length;
-    }
-    prodHeroImgOut.src = a[count - 1].src
-  })
-  pr.addEventListener('click', () => {
-    let a = Array.from(document.querySelector('.get-imgs').querySelectorAll('img'));
-    count += 1;
-    if (count === a.length + 1) {
-      count = 1;
-    }
-    prodHeroImgOut.src = a[count - 1].src
-  })
+    })
+  }
+  if (pl) {
+    pl.addEventListener('click', () => {
+      let a = Array.from(document.querySelector('.get-imgs').querySelectorAll('img'));
+      count -= 1;
+      if (count === 0) {
+        count = a.length;
+      }
+      prodHeroImgOut.src = a[count - 1].src
+    })
+  }
+  if (pr) {
+    pr.addEventListener('click', () => {
+      let a = Array.from(document.querySelector('.get-imgs').querySelectorAll('img'));
+      count += 1;
+      if (count === a.length + 1) {
+        count = 1;
+      }
+      prodHeroImgOut.src = a[count - 1].src
+    })
+  }
+
 })
